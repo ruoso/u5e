d0040b68813a32ab0553fa4bf2dbd40900ec0d9d
Daniel Ruoso <daniel@ruoso.com>
2015-09-14 23:24:05 -0400

    Create utf32_native.hpp
    utf32_native is the simplest of the encodings, because it is a direct wrapper over the memory representation.

---
+++ include/u5e/enc/utf32_native.hpp
@@ -0,0 +1,27 @@
+#ifndef INCLUDED_U5E_ENC_UTF32_NATIVE_HPP
+#define INCLUDED_U5E_ENC_UTF32_NATIVE_HPP
+
+#include <u5e/buffer.hpp>
+#include <u5e/codepoint.hpp>
+
+namespace u5e::enc {
+  /**
+   * u5e::enc::utf32_native
+   *
+   * The native utf32 encoding is built with the native 32 bit
+   * integer type. It is specially more useful for cases where
+   * you need to do extensive manipulation on text, since it
+   * allows you to have constant random access time.
+   */
+  class utf32_native {
+    typedef u5e::buffer<u5e::codepoint> enc_buffer_type;
+    typedef enc_buffer_type::iterator iterator;
+    typedef enc_buffer_type::reverse_iterator reverse_iterator;
+    static const enc_buffer_type::size_type min_codepoint_size = 1;
+    static const enc_buffer_type::size_type max_codepoint_size = 1;
+    static const enc_buffer_type::size_type max_bmp_codepoint_size = 1;
+    static const enc_buffer_type::size_type max_ascii_codepoint_size = 1;
+  }
+}
+
+#endif

2a51363070b49126987323c7ff0e6acf48b944fc
Daniel Ruoso <daniel@ruoso.com>
2016-03-13 21:06:25 -0400

    we can use the iterator straight from string_view

--- examples/simple_iteration.cpp
+++ examples/simple_iteration.cpp
@@ -1,6 +1,5 @@
 
 #include <experimental/string_view>
-#include <u5e/codepoint.hpp>
 #include <u5e/utf8.hpp>
 #include <stdio.h>
 
@@ -9,9 +8,8 @@ using std::experimental::string_view;
 int main(int argc, char **argv) {
   for (int i = 1; i < argc; i++) {
     string_view p(argv[i], strlen(argv[i]));
-    u5e::utf8<string_view> parameter(p);
-    u5e::utf8<string_view>::iterator it = parameter.begin();
-    while (it != parameter.end()) {
+    u5e::utf8<string_view>::iterator it = p.begin();
+    while (it != p.end()) {
       printf(" U+%06llx ", *it++);
     }
     printf("\n");
